be2010898c23d6b2fe7078b43f476ffceb34d657
Dalo <pastor.tom.etu@gmail.com>
2019-11-28 12:11:39 +0100

    Ajout Podium
    Ajout du systeme de podium stocké dans la mémoire du PC

---
+++ Assets/Scenes/PodiumScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PodiumScript : MonoBehaviour
+{
+    public GameObject ScoreText;
+    public string place;
+    void Start()
+    {
+        Text txt = ScoreText.GetComponent<Text>();
+        if (PlayerPrefs.HasKey("Player"+place))
+        {
+            txt.text += "1." + PlayerPrefs.GetString("Player"+place);
+        }
+    }
+}
