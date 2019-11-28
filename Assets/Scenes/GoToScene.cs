949acb3d92e711096276d3fa36759fd87046a4d1
Dalo <pastor.tom.etu@gmail.com>
2019-11-28 09:59:14 +0100

    ajout Scene Menu
    Bouton pour jouer + tableau des meilleurs score stockés et récupérés dans la mémoire du PC

---
+++ Assets/Scenes/GoToScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour
+{
+    public string scenename;
+    // Start is called before the first frame update
+    public void ChangerScene()
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
---
+++ Assets/Scenes/HighScoreScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreScript : MonoBehaviour
+{
+    // Récupère les meilleurs scores dans la mémoire et les affiche
+    public GameObject ScoreText;
+    void Start()
+    {
+        Text txt = ScoreText.GetComponent<Text>();
+        txt.text = "Meilleur Score \n";
+        if (PlayerPrefs.HasKey("Player1"))
+        {
+            txt.text += "1."+PlayerPrefs.GetString("Player1")+"\n\n";
+        }
+        if (PlayerPrefs.HasKey("Player2"))
+        {
+            txt.text += "2."+PlayerPrefs.GetString("Player2")+"\n";
+        }
+        if (PlayerPrefs.HasKey("Player3"))
+        {
+            txt.text += "3."+PlayerPrefs.GetString("Player3")+"\n";
+        }
+        if (PlayerPrefs.HasKey("Player4"))
+        {
+            txt.text += "4." + PlayerPrefs.GetString("Player4") + "\n";
+        }
+        if (PlayerPrefs.HasKey("Player5"))
+        {
+            txt.text += "5." + PlayerPrefs.GetString("Player5") + "\n";
+        }
+    }
+}
